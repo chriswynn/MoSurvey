9098de47af339764d3259ca818eada075673e9b9
chriswynn <chris@wynncodes.com>
2020-02-21 10:37:24 -0500

    feat: add ueberauth to extra_applications

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Mosurvey.MixProject do
   def application do
     [
       mod: {Mosurvey.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ueberauth, :ueberauth_google]
     ]
   end
 
